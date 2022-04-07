1ec05ee35c3f70bce1427867b70ff37d65724a33
Denys Shevel <denys.shevel16@gmail.com>
2022-04-07 11:07:05 -0400

    Add moving gradient name

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import Button from './Button'
+import Name from './Name'
 
 function App() {
   return (
     <div className="App">
+      <div className="section fullscreen">
+        <div>
+          <p>Hi there, I'm</p>
+          <Name></Name>
+          <Button text="Abou me"></Button>
+        </div>
+      </div>
+      <div class="section fullscreen" id="about-me">
+      </div>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,8 @@
+export default function Button(props) {
+    return (
+        <div class="Button">
+            <div class="light"></div>
+            <div class="text">{props.text}</div>
+        </div>
+    )
+}
